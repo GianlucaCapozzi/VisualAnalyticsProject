384344cfea9c29c52c9f12db3798d8a78ab97d61
Marco <costa.1691388@studenti.uniroma1.it>
2020-03-06 01:10:30 +0100

    Add fixed position to logo, added folder structure

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+var dataset = "src/formula-1-race-data";
+var circuits = dataset.concat("/circuits.csv");
+
+$("body").css("background-image", "url(src/images/f1_logo.png)");
+$("body").css("background-repeat", "no-repeat");
+$("body").css("background-attachment", "fixed");
+$("body").css("background-position", "2% 2%");
