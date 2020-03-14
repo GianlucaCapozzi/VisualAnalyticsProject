afb4182cf2f2da7f8082fe5445a465ef3b356566
Gianluca Capozzi <capozzigianluca96@gmail.com>
2020-03-14 11:15:38 +0100

    Fix bestLaps code

--- src/formula-1-race-data/dbForPCA.py
+++ src/formula-1-race-data/dbForPCA.py
@@ -13,6 +13,27 @@ poles = mergedRacQual[is_first_pos]
 #print(poles)
 
 mergedPolesCirc = poles.merge(circuits, on="circuitId").filter(["name", "year", "q1", "q2", "q3"])
-print(mergedPolesCirc)
+#print(mergedPolesCirc.iloc[0, :])
 
-mergedPolesCirc.to_csv("bestLaps.csv", sep="\t")
+mergeRows = mergedPolesCirc.shape[0]
+
+#print(mergeRows)
+notQ3 = 0
+
+for i in range(0, mergedPolesCirc.shape[0]):
+    #print(mergedPolesCirc.iloc[i, 4])
+    if (mergedPolesCirc.iloc[i, 4] == "\\N"):
+        if(mergedPolesCirc.iloc[i, 3] != "\\N"):
+            mergedPolesCirc.iloc[i, 4] = mergedPolesCirc.iloc[i, 3]
+        else:
+            mergedPolesCirc.iloc[i, 4] = mergedPolesCirc.iloc[i, 2]
+        #print(mergedPolesCirc.iloc[i, 4])
+    #if (mergedPolesCirc.iloc[i, 4] == "\\N"):
+    #    notQ3 = notQ3 + 1
+#print(notQ3)
+
+bestLaps = mergedPolesCirc.filter(["name", "year", "q3"])
+
+print(bestLaps)
+
+bestLaps.to_csv("bestLaps.csv")
